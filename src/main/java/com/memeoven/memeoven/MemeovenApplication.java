e6988cb2033ff0bac19207c0cb47011150b8f8a5
Yevgenya Suvorova <yevgenya.suvorova08@gmail.com>
2023-05-16 21:36:28 +0300

    Initialized memeoven

---
+++ src/main/java/com/memeoven/memeoven/MemeovenApplication.java
@@ -0,0 +1,13 @@
+package com.memeoven.memeoven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeovenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeovenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/memeoven/memeoven/MemeovenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.memeoven.memeoven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeovenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
