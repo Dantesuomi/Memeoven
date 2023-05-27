b00529cd21424774aaff85b2aa6fdaf649342280
Yevgenya Suvorova <yevgenya.suvorova08@gmail.com>
2023-05-27 12:46:25 +0300

    Added not null user not null comment

--- src/main/java/com/memeoven/memeoven/meme/Comment.java
+++ src/main/java/com/memeoven/memeoven/meme/Comment.java
@@ -4,6 +4,7 @@ package com.memeoven.memeoven.meme;
 import com.memeoven.memeoven.entity.User;
 import com.memeoven.memeoven.meme.Meme;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,9 +22,11 @@ public class Comment {
     private Long id;
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @NotNull
     private User user;
     @ManyToOne
     @JoinColumn(name = "meme_id")
+    @NotNull
     private Meme meme;
     @Column(columnDefinition="TEXT")
     private String comment;
